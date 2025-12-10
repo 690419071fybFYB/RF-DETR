844bc0203afd07b34df99412dbda62b313678e70
fybhlhaaa <fybhlhaaa@163.com>
2025-12-11 00:25:24 +0800

    fix: Fix UNet Up module channel mismatch in density_init_improved
    Changed DoubleConv input from in_channels to in_channels + out_channels
    to correctly handle the concatenated skip connection features.

--- rfdetr/models/density_init_improved.py
+++ rfdetr/models/density_init_improved.py
@@ -60,7 +60,9 @@ class Up(nn.Module):
         super().__init__()
         if bilinear:
             self.up = nn.Upsample(scale_factor=2, mode='bilinear', align_corners=True)
-            self.conv = DoubleConv(in_channels, out_channels, in_channels // 2)
+            # After concat: in_channels + skip_channels, where skip_channels = out_channels
+            # So total input to conv = in_channels + out_channels
+            self.conv = DoubleConv(in_channels + out_channels, out_channels)
         else:
             self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2, stride=2)
             self.conv = DoubleConv(in_channels, out_channels)
